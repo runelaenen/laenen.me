95a8f541ef86755554961e02fd18b7e24cffdc94
Rune Laenen <rune.laenen@phpro.be>
2020-08-22 10:30:36 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rune Laenen</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+</head>
+<body>
+<div class="gradient"></div>
+<div class="card">
+    <div class="card--header">
+        <div class="card--header--orb"></div>
+        <div class="card--header--orb"></div>
+        <div class="card--header--orb"></div>
+    </div>
+    <div class="card--body">
+        <img src="RuneLaenen.png" class="image" />
+        <h1>Rune Laenen</h1>
+        <span>Magento & Shopware certified<br />Web developer</span>
+        <ul>
+            <li><a href="https://linkedin.com/in/runelaenen" target="_blank" class="fab fa-linkedin" name="LinkedIn"></a></li>
+            <li><a href="https://instagram.com/runelaenen" target="_blank" class="fab fa-instagram" name="Instagram"></a></li>
+            <li><a href="https://m.me/runelaenen" target="_blank" class="fab fa-facebook-messenger" name="Facebook Messenger"></a></li>
+            <li><a href="https://twitter.com/runelaenen" target="_blank" class="fab fa-twitter" name="Twitter"></a></li>
+            <li><a href="mailto:rune@laenen.nu" target="_blank" class="fal fa-envelope-open-text" name="Email"></a></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-82044964-3"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-82044964-3');
+</script>
+
+</body>
+</html>
